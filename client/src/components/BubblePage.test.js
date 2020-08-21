adb2ded475f4b2938bf900b9a1b4a62c6c6b78c2
timotheemcdonald <timotheemcdonald@gmail.com>
2020-08-21 13:54:58 -0400

    submitting not finished

--- client/src/components/BubblePage.test.js
+++ client/src/components/BubblePage.test.js
@@ -1,7 +1,37 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import BubblePage from "./BubblePage";
+import BubblePage from "./BubblePage.js";
+
+const bubbleData = [
+  {
+    code: {hex: "#f0f8ff"},
+    color: "aliceblue",
+    id: 1
+  },
+  {
+    code: {hex: "#99ddbc"},
+    color: "limegreen",
+    id: 2
+  },
+  {
+    code: {hex: "#00ffff"},
+    color: "aqua",
+    id: 3
+  }
+]
+
 
 test("Fetches data and renders the bubbles", () => {
   // Finish this test
+
+  const {rerender} = render(<BubblePage colorList={[]} />)
+
+  let episodesArray = screen.queryAllByTestId(/colors/i)
+  expect(episodesArray).toHaveLength(0)
+
+  rerender(<BubblePage colorList={bubbleData}/>)
+
+  episodesArray = screen.getAllByTestId(/colors/i)
+  expect(episodesArray).toHaveLength(3)
+
 });
